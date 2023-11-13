b467d4812475e5cc1ebab69b3d65ccaf45ab4371
Giang <463189@student.fontys.nl>
2023-11-13 21:49:22 +0100

    initial project

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+#pragma region [Car]
+class Car {
+public:
+    //constructure
+    explicit Car(const string &model) : model(model) {
+        cout << "Car " << model << " is created." << endl;
+    }
+    //Destructure
+    ~Car() {
+        cout << "Car " << model << " is destroyed." << endl;
+    }
+
+    void drive() const {
+        cout << "Driving the car " << model << "." << endl;
+    }
+
+private:
+    string model;
+};
+
+// Function that returns a shared_ptr to a Car
+shared_ptr<Car> createCar(const string &model) {
+    return make_shared<Car>(model);
+}
+#pragma endregion Car
+
+int main() {
+#pragma region [Dynamic memory allocation using raw and unique pointers]
+    cout << "___Dynamic memory allocation using raw and unique pointers___" << endl;
+    // int* dynamicArray = new int[5];
+    unique_ptr<int[]> dynamicArray = make_unique<int[]>(5);
+
+    // Initialize values
+    for (int i = 0; i < 5; ++i) {
+        dynamicArray[i] = i * 10;
+    }
+
+    // Access and print values
+    for (int i = 0; i < 5; ++i) {
+        cout << "Value at index " << i << ": " << dynamicArray[i] << endl;
+    }
+
+    // delete[] dynamicArray;
+#pragma endregion [Dynamic memory allocation using raw and unique pointers]
+
+#pragma region [Class (auto private and public) and shared ptr]
+    cout << "_________Class (auto private and public) and shared ptr_________" << endl;
+    // Creating a shared_ptr to a Car
+    const shared_ptr<Car> giangCar = createCar("RS e-tron GT");
+
+    // Creating another shared_ptr that shares ownership
+    const shared_ptr<Car> amyCar = giangCar;
+
+    // Using the shared_ptr(s)
+    giangCar->drive();
+    amyCar->drive();
+#pragma endregion [Class (auto private and public) and shared ptr]
+
+    return 0;
+}
