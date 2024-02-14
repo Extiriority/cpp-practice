3d98d11e7761b69a0a5869d262a46a8cec7b8122
Giang <463189@student.fontys.nl>
2024-02-14 21:21:10 +0100

    Add TTT to games

---
+++ src/Games/TicTacToe.cpp
@@ -0,0 +1,85 @@
+//
+// Created by giang on 14 Feb 2024.
+//
+
+#include "TicTacToe.h"
+
+void TicTacToe::Update() const {
+    cout << "      _______________" << '\n';
+    cout << "          1   2   3" << '\n';
+    cout << "       A  "<<coordMap.at("A1")<<" | "<<coordMap.at("A2")<<" | "<<coordMap.at("A3")<<" " << '\n';
+    cout << "         -----------" << '\n';
+    cout << "       B  "<<coordMap.at("B1")<<" | "<<coordMap.at("B2")<<" | "<<coordMap.at("B3")<<" " << '\n';
+    cout << "         -----------" << '\n';
+    cout << "       C  "<<coordMap.at("C1")<<" | "<<coordMap.at("C2")<<" | "<<coordMap.at("C3")<<" " << '\n';
+    cout << "      _______________" << '\n';
+}
+
+void TicTacToe::SwitchPlayer() { playerMark = playerMark == 'X' ? 'O' : 'X'; }
+
+void TicTacToe::Input() {
+    do {
+        cout << "Player ["<<playerMark<<"], enter your move: ";
+        cin >> userLastMove;
+        cout << '\n';
+
+        if (coordMap.contains(userLastMove) && coordMap[userLastMove] == ' ') {
+            coordMap[userLastMove] = playerMark;
+            break;
+        }
+        cout << "    ___Invalid input___" << '\n';
+        cout << "available OR correct coords ";
+    } while (true);
+}
+
+void TicTacToe::nextRound() {
+    turnCounter++;
+    SwitchPlayer();
+}
+
+bool TicTacToe::CheckWin() const {
+    return CheckRow(userLastMove[0]) || CheckCol(userLastMove[1]) || CheckDiagonals();
+}
+
+bool TicTacToe::CheckRow(const char rowChar) const {
+    const string row(1,  rowChar);
+    return coordMap.at(row + "1") == playerMark &&
+           coordMap.at(row + "2") == playerMark &&
+           coordMap.at(row + "3") == playerMark;
+}
+
+bool TicTacToe::CheckCol(const char colChar) const {
+    const string col(1,  colChar);
+    return coordMap.at("A" + col) == playerMark &&
+           coordMap.at("B" + col) == playerMark &&
+           coordMap.at("C" + col) == playerMark;
+}
+
+bool TicTacToe::CheckDiagonals() const {
+    const bool diagonalHill =  coordMap.at("A1") == playerMark &&
+                                   coordMap.at("B2") == playerMark &&
+                                   coordMap.at("C3") == playerMark;
+    const bool diagonalSlope = coordMap.at("A3") == playerMark &&
+                               coordMap.at("B2") == playerMark &&
+                               coordMap.at("C1") == playerMark;
+    return diagonalHill || diagonalSlope;
+}
+
+void TicTacToe::PromptResetOrExit() {
+    cout << "Try again Y/N?: ";
+    cin >> userLastMove;
+
+    if (userLastMove == "Y") {
+        winState = false;
+        turnCounter = 0;
+
+        for (auto& [coords, input] : coordMap) {
+            input = ' ';
+        }
+        cout << '\n';
+        Update();
+        SwitchPlayer();
+    } else {
+        cout << "___ Thanks for Playing me! ___" << '\n';
+    }
+}
---
+++ src/Games/TicTacToe.h
@@ -0,0 +1,37 @@
+//
+// Created by giang on 14 Feb 2024.
+//
+
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using std::string, std::cout, std::cin, std::map;
+
+class TicTacToe {
+public:
+    char playerMark = 'X';
+    string userLastMove;
+    int turnCounter = 0;
+    bool winState = false;
+    map<string, char> coordMap = {
+        {"A1", ' '}, {"A2", ' '}, {"A3", ' '},
+        {"B1", ' '}, {"B2", ' '}, {"B3", ' '},
+        {"C1", ' '}, {"C2", ' '}, {"C3", ' '}
+    };
+
+    void Update() const;
+    void SwitchPlayer();
+    void Input();
+    void nextRound();
+    [[nodiscard]] bool CheckWin() const;
+    [[nodiscard]] bool CheckRow(char rowChar) const;
+    [[nodiscard]] bool CheckCol(char colChar) const;
+    [[nodiscard]] bool CheckDiagonals() const;
+    void PromptResetOrExit();
+};
+
+#endif //TICTACTOE_H
