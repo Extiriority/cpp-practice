6cb67c0948655bf8cbe37d10fe1a3c270ec64239
Giang <vhg.trang@gmail.com>
2024-02-24 22:21:12 +0100

    Abstract

--- src/Advent_of_Code/Day1/Not_Quite_Lisp.cpp
+++ src/Advent_of_Code/Day1/Not_Quite_Lisp.cpp
@@ -3,20 +3,23 @@
 //
 
 #include "Not_Quite_Lisp.h"
+
+#include <cstdint>
 #include <string>
+
+using namespace std;
 
 enum Floor { GROUND_LEVEL = 0, BASEMENT_LEVEL = -1 };
 
-int Not_Quite_Lisp::WhichFloor(const std::string& apartment) {
-    int floorPosition {};
-    constexpr int GROUND_LEVEL {};
-    constexpr int BASEMENT_LEVEL {-1};
+tuple<int16_t, auto> Not_Quite_Lisp::WhichFloor(const std::string& apartment) {
+    int16_t floorPosition = 0;
+    auto floorsToBasement = 0;
 
     for (int currentFloorLevel = GROUND_LEVEL; currentFloorLevel < apartment.size(); currentFloorLevel++) {
         floorPosition += apartment[currentFloorLevel] == '(' ? 1 : -1;
         if (floorPosition == BASEMENT_LEVEL) {
-            return currentFloorLevel + 1;
+            floorsToBasement = currentFloorLevel + 1;
         }
     }
-    return floorPosition;
+    return make_tuple(floorPosition, floorsToBasement);
 }
--- src/Advent_of_Code/Day2/I_Was_Told_There_Would_Be_No_Math.cpp
+++ src/Advent_of_Code/Day2/I_Was_Told_There_Would_Be_No_Math.cpp
@@ -1,5 +1,35 @@
 //
 // Created by giang on 14 Feb 2024.
 //
-
 #include "I_Was_Told_There_Would_Be_No_Math.h"
+
+#include <cstdint>
+#include <sstream>
+
+using namespace std;
+
+tuple<uint32_t, uint32_t> I_Was_Told_There_Would_Be_No_Math::CalculateTotalWrapping(const string& input) {
+    istringstream ss(input);
+    string LENGTH, WIDTH, HEIGHT;
+    uint32_t totalPaperInSquareFeet = 0;
+    uint32_t totalLintInSquareFeet = 0;
+
+    while (getline(ss, LENGTH, 'x') && getline(ss, WIDTH, 'x') && getline(ss, HEIGHT, '\n')) {
+        int dimension[3] {stoi(LENGTH), stoi(WIDTH), stoi(HEIGHT)};
+
+        for (auto swapIndex = 0; swapIndex < 3; swapIndex++) {
+            for (auto compareIndex = swapIndex + 1; compareIndex < 3; compareIndex++) {
+                if (dimension[swapIndex] > dimension[compareIndex])
+                    swap(dimension[swapIndex], dimension[compareIndex]);
+            }
+        }
+        const auto volume = 2*(dimension[0]*dimension[1] + dimension[1]*dimension[2] + dimension[2]*dimension[0]);
+        const auto extra = dimension[0] * dimension[1];
+        totalPaperInSquareFeet += volume + extra;
+
+        const auto ribbon = 2*(dimension[0] + dimension[1]);
+        const auto bow = dimension[0] * dimension[1] * dimension[2];
+        totalLintInSquareFeet += ribbon + bow;
+    }
+    return make_tuple(totalPaperInSquareFeet, totalLintInSquareFeet);
+}
